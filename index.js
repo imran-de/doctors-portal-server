903c7e895b3f2952f2e7ff09b70769e68e6bf4d8
imran-de <stimran4405@gmail.com>
2021-11-06 11:08:38 +0600

    api

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, MongoRuntimeError } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5okll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,7 +18,24 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("doctors_portal");
-        const collection = database.collection("appointment")
+        const appointmentsCollection = database.collection("appointments");
+
+        //post appointment 
+        app.post('/appointments', async (req, res) => {
+            const data = req.body;
+            const result = await appointmentsCollection.insertOne(data);
+            res.json(result);
+        });
+        //get appointment
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email, date: date }
+            const cursor = appointmentsCollection.find(query);
+            const result = await cursor.toArray();
+            res.json(result);
+        })
+
 
     }
     finally {
@@ -35,3 +53,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening at port ${port}`);
 })
+
+/*
+        //users: get
+        //users: post
+        //users: put "update user information"
+        //users: delete
+*/
